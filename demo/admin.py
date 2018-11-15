63c2597d4431ff9ec62e35517f6255cdf6e7f132
kyra <kyrantonia@gmail.com>
2018-11-15 20:13:49 -0300

    Crear model usuarios

--- demo/admin.py
+++ demo/admin.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from demo.models import PersonaNatural
+from demo.models import Admin
+
+admin.site.register(PersonaNatural)
+admin.site.register(Admin)
--- demo/models.py
+++ demo/models.py
@@ -2,5 +2,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Usuario(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  rut = models.IntegerField()
+  email = models.EmailField()
+
+  def __str__(self):
+    return f'{self.email}-{self.rut}'
+
+class PersonaNatural(Usuario):
+  def is_admin(self):
+    return False
+
+class Admin(Usuario):
+  def is_admin(self):
+    return True
