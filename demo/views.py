7c961c5be4cdca56bed69b3ee6e34ff79b8c5926
kparga <karina.parga94@gmail.com>
2018-11-20 20:09:03 -0300

    agregado clases a views.py

--- demo/views.py
+++ demo/views.py
@@ -10,17 +10,26 @@ from django.views.generic import TemplateView
 class LoginView(TemplateView):
     template_name = 'login.html'
 
+class Coevaluacion(TemplateView):
+    pass
+
 class CoevaluacionAlumnoView(TemplateView):
     template_name = 'coevaluacion-vista-alumno.html'
 
 class CoevaluacionDocenteView(TemplateView):
     template_name = 'coevaluacion-vista-docente.html'
 
+class Curso(TemplateView):
+    pass
+
 class CursoAlumnoView(TemplateView):
     template_name = 'curso-vista-alumno.html'
 
 class CursoDocenteView(TemplateView):
     template_name = 'curso-vista-docente.html'
+
+class Home(TemplateView):
+    pass
 
 class HomeAlumnoView(TemplateView):
     template_name = 'home-vista-alumno.html'
@@ -30,6 +39,9 @@ class HomeDocenteView(TemplateView):
 
 class HomeProfesorView(TemplateView):
     template_name = 'home-vista-profesor.html'
+
+class Perfil(TemplateView):
+    pass
 
 class PerfilDuenoView(TemplateView):
     template_name = 'perfil-vista-dueno.html'
