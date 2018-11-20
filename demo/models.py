d7ff6c381ca22573d0d9113214a6c2554b840238
kparga <karina.parga94@gmail.com>
2018-11-20 18:41:59 -0300

    espacio

--- demo/models.py
+++ demo/models.py
@@ -103,7 +103,6 @@ class InstanciaCoevaluacion(models.Model):
     fecha_respuesta = models.DateField(default=None)
     respondida = models.BooleanField(default=False)
     respuesta = models.ForeignKey('Respuesta', on_delete=models.CASCADE)
-    
 
     def __str__(self):
         return f'{self.fecha_respuesta}'
