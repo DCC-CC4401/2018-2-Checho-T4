271ffb39461c033bcae9f513c9b9fe407c90cc5f
kparga <karina.parga94@gmail.com>
2018-11-20 19:48:40 -0300

    agregada clases a views.py eliminada demo.html

--- demo/views.py
+++ demo/views.py
@@ -6,8 +6,36 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-class DemoView(TemplateView):
-    template_name = 'demo.html'
 
 class LoginView(TemplateView):
     template_name = 'login.html'
+
+class CoevaluacionAlumnoView(TemplateView):
+    template_name = 'coevaluacion-vista-alumno.html'
+
+class CoevaluacionDocenteView(TemplateView):
+    template_name = 'coevaluacion-vista-docente.html'
+
+class CursoAlumnoView(TemplateView):
+    template_name = 'curso-vista-alumno.html'
+
+class CursoDocenteView(TemplateView):
+    template_name = 'curso-vista-docente.html'
+
+class HomeAlumnoView(TemplateView):
+    template_name = 'home-vista-alumno.html'
+
+class HomeDocenteView(TemplateView):
+    template_name = 'home-vista-docente.html'
+
+class HomeProfesorView(TemplateView):
+    template_name = 'home-vista-profesor.html'
+
+class PerfilDuenoView(TemplateView):
+    template_name = 'perfil-vista-dueno.html'
+
+class PerfilAlumnoDocenteView(TemplateView):
+    template_name = 'perfil-alumno-vista-docente.html'
+
+
+
